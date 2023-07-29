ea5514e872cee1aad3c43c709d1c364d9444c0fd
adrian chriswanto <adrian.chriswanto@gmail.com>
2023-07-29 17:03:01 +0700

    update

--- dags/sources/postgres.py
+++ dags/sources/postgres.py
@@ -3,6 +3,7 @@ from sources.base import Source
 class PostgresSource(Source):
 
     template_fields = ("sql", )
+    template_ext = ".sql"
 
     def __init__(self, sql, *args, **kwargs):
         super(PostgresSource, self).__init__(*args, **kwargs)
